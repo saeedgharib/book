b5ce815cdb19a8dcf01f2fc6f058cff0f23c4a8a
Muhammad Saeed <71532695+saeedgharib@users.noreply.github.com>
2025-01-05 23:20:19 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://rimshamuneer126:SxtRmYn6o2ZjQ3fB@cluster0.ix1gx.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
