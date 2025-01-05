fbb11423baad2d3f872db68efbb468255caa2a1f
Muhammad Saeed <71532695+saeedgharib@users.noreply.github.com>
2025-01-06 00:17:26 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
+import { PORT, mongoDBURL } from '../config.js';
 import mongoose from 'mongoose';
-import booksRoute from './routes/booksRoute.js';
+import booksRoute from './/routes/booksRoute.js';
 import cors from 'cors';
 
 const app = express();
